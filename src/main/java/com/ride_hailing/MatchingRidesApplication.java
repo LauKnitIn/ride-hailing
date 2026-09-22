5a25a7bc85f7af2eeb9631fa58ed2172fc4f26d9
Laura Correa <correabsofial@gmail.com>
2026-09-21 22:49:15 -0500

    feat: baseline structure

---
+++ src/main/java/com/ride_hailing/MatchingRidesApplication.java
@@ -0,0 +1,13 @@
+package com.ride_hailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingRidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingRidesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ride_hailing/MatchingRidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ride_hailing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingRidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
